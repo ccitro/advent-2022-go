2b0fe14498c9213c04d4cf62d39581236a375ffb
Craig Citro <craig@citro.net>
2023-07-11 18:17:16 -0500

    Implement A* to solve part1

--- day12/day12.go
+++ day12/day12.go
@@ -14,6 +14,8 @@ type heightmap struct {
 	maxX    int
 	start   int
 	end     int
+	endX    int
+	endY    int
 }
 
 func coordsToIndex(x, y, maxX int) int {
@@ -36,8 +38,9 @@ func loadHeightmap(file *os.File) *heightmap {
 				v = 'a'
 			} else if v == 'E' {
 				hm.end = coordsToIndex(i, y, hm.maxX)
+				hm.endX = i
+				hm.endY = y
 				v = 'z'
-				// v = 'l'
 			}
 
 			hm.terrain = append(hm.terrain, int(v-'a'))
@@ -48,80 +51,138 @@ func loadHeightmap(file *os.File) *heightmap {
 	return &hm
 }
 
-func pathContainsPoint(p *path, pt int) bool {
-	for _, v := range *p {
-		if v == pt {
-			return true
+func reconstructPath(cameFrom map[int]int, current int, origin int) *path {
+	maxLength := len(cameFrom)
+	totalPath := path{current}
+	for {
+		if _, ok := cameFrom[current]; !ok {
+			break
+		}
+		if current == origin {
+			break
+		}
+		current = cameFrom[current]
+		totalPath = append(totalPath, current)
+		if len(totalPath) > maxLength {
+			panic("Path too long")
 		}
 	}
-	return false
+	return &totalPath
 }
 
-func findShortestPath(hm *heightmap) *path {
-	defaultPath := path{hm.start}
-	pathsLeft := []*path{&defaultPath}
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
 
+func heuristicCostEstimate(p int, hm *heightmap) int {
+	x := p % hm.maxX
+	y := p / hm.maxX
+
+	return abs(x-hm.endX) + abs(y-hm.endY)
+}
+
+func getNeighbors(current int, hm *heightmap) []int {
+	neighbors := []int{}
 	maxX := hm.maxX
 	maxIndex := len(hm.terrain) - 1
-	for len(pathsLeft) > 0 {
-		fmt.Printf("Paths left: %d\n", len(pathsLeft))
-		thisPath := pathsLeft[0]
-		pathsLeft = pathsLeft[1:]
-
-		currentPoint := (*thisPath)[len(*thisPath)-1]
-		currentHeight := hm.terrain[currentPoint]
-		if currentPoint == hm.end {
-			return thisPath
-		}
-
-		for i := 0; i < 4; i++ {
-			nextPoint := currentPoint
-			if i == 0 {
-				// east
-				nextPoint++
-				if nextPoint%maxX == 0 {
-					continue
-				}
-			} else if i == 1 {
-				// south
-				nextPoint += maxX
-				if nextPoint > maxIndex {
-					continue
-				}
-			} else if i == 2 {
-				// west
-				nextPoint--
-				if nextPoint%maxX == maxX-1 || nextPoint < 0 {
-					continue
-				}
-			} else if i == 3 {
-				// north
-				nextPoint -= maxX
-				if nextPoint < 0 {
-					continue
-				}
-			}
-
-			if pathContainsPoint(thisPath, nextPoint) {
+	currentHeight := hm.terrain[current]
+	for i := 0; i < 4; i++ {
+		nextPoint := current
+		if i == 0 {
+			// east
+			nextPoint++
+			if nextPoint%maxX == 0 {
 				continue
 			}
-
-			nextHeight := hm.terrain[nextPoint]
-			heightDifference := nextHeight - currentHeight
-			if heightDifference > 1 {
+		} else if i == 1 {
+			// south
+			nextPoint += maxX
+			if nextPoint > maxIndex {
 				continue
 			}
+		} else if i == 2 {
+			// west
+			nextPoint--
+			if nextPoint%maxX == maxX-1 || nextPoint < 0 {
+				continue
+			}
+		} else if i == 3 {
+			// north
+			nextPoint -= maxX
+			if nextPoint < 0 {
+				continue
+			}
+		}
+		nextPointHeight := hm.terrain[nextPoint]
+		heightDifference := nextPointHeight - currentHeight
+		if heightDifference > 1 {
+			continue
+		}
+		neighbors = append(neighbors, nextPoint)
+	}
 
-			newPath := make(path, len(*thisPath))
-			copy(newPath, *thisPath)
-			newPath = append(newPath, nextPoint)
+	return neighbors
+}
 
-			pathsLeft = append(pathsLeft, &newPath)
+func getNeighborWeight(current int, neighbor int, hm *heightmap) int {
+	currentHeight := hm.terrain[current]
+	neighborHeight := hm.terrain[neighbor]
+	return 2 - (neighborHeight - currentHeight)
+}
+
+func A_Star(start int, goal int, hm *heightmap) *path {
+	inspections := 0
+	openSet := make(map[int]bool)
+	openSet[start] = true
+
+	cameFrom := make(map[int]int)
+	gScore := make(map[int]int)
+	gScore[start] = 0
+
+	fScore := make(map[int]int)
+	fScore[start] = heuristicCostEstimate(start, hm)
+
+	for len(openSet) > 0 {
+		current := -1
+		for k, _ := range openSet {
+			if current == -1 || fScore[k] < fScore[current] {
+				current = k
+			}
 		}
 
+		if current == goal {
+			fmt.Printf("Inspected %d nodes\n", inspections)
+			return reconstructPath(cameFrom, current, start)
+		}
+
+		delete(openSet, current)
+		for _, neighbor := range getNeighbors(current, hm) {
+			inspections++
+			tentative_gScore := gScore[current] + getNeighborWeight(current, neighbor, hm)
+			neighborScore := gScore[neighbor]
+			if neighborScore == 0 {
+				neighborScore = 999999999
+			}
+
+			if tentative_gScore < neighborScore {
+				cameFrom[neighbor] = current
+				gScore[neighbor] = tentative_gScore
+				fScore[neighbor] = gScore[neighbor] + heuristicCostEstimate(neighbor, hm)
+				if !openSet[neighbor] {
+					openSet[neighbor] = true
+				}
+			}
+		}
 	}
 
 	panic("No path found")
+}
+
+func findShortestPath(hm *heightmap) *path {
+	return A_Star(hm.start, hm.end, hm)
 }
 
 func (hm *heightmap) print() {
@@ -146,7 +207,8 @@ func part1(file *os.File) {
 	hm.print()
 
 	path := findShortestPath(hm)
-	fmt.Printf("Found path of length %d\n", len(*path))
+	fmt.Printf("Found path with %d steps\n", len(*path)-1)
+	println()
 }
 
 func part2(file *os.File) {
